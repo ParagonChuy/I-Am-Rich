acd9b8b0673757eb062a33b7b76f4d246d0cbb4c
jesus vega <jesusvega@jesuss-MacBook-Pro.local>
2018-12-04 12:37:01 -0800

    Initialize project

---
+++ I Am Rich App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich App
+//
+//  Created by jesus vega on 12/4/18.
+//  Copyright © 2018 jesus vega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
